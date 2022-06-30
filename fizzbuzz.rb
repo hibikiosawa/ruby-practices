a17c1c1df36d8d4003f82ec2988c73bd8f987c5b
hibikiosawa <105911942+hibikiosawa@users.noreply.github.com>
2022-06-30 10:17:32 +0900

    update fizzbuzz.rb
    変更

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,5 @@
-num = 0
-while num < 20 
-    num = num + 1
+num = 1
+while num <= 20 
     if num % 3 == 0 && num % 5 == 0
         puts "FizzBuzz"
     elsif num % 3 == 0
@@ -10,4 +9,5 @@ while num < 20
     else
         puts "#{num}"
     end
+    num = num + 1
 end
