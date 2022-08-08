2a13d5c73f6063d7a5284ec658b07be180ff597d
hibikiosawa <105911942+hibikiosawa@users.noreply.github.com>
2022-08-08 22:33:46 +0900

    リスト課題4の提出

--- 05.ls/ls2.rb
+++ 05.ls/ls2.rb
@@ -1,16 +1,80 @@
 # frozen_string_literal: true
+
 require 'optparse'
+require 'etc'
 
 opt = OptionParser.new
 args = {}
-opt.on('-a') {|v| args[:a] = v}
+opt.on('-l') { |v| args[:l] = v }
 opt.parse!(ARGV)
 
-def input(args)
-  if args[:a].nil?
-    Dir.glob('*')
-  else
-    Dir.glob('.*') + Dir.glob('*')
+def input(_args)
+  Dir.glob('*')
+end
+
+def count(arr)
+  nlinkmax = 0
+  sizemax = 0
+  files = arr.size
+  files.times do |row|
+    fs = File::Stat.new(arr[row])
+    nlinkmax = fs.nlink if nlinkmax < fs.nlink
+    sizemax = fs.size if sizemax < fs.size
+  end
+  @nlinkmax = nlinkmax.to_s.length + 1
+  @sizemax = sizemax.to_s.length + 1
+end
+
+def file_convert(fil)
+  file = {
+    '010' => 'p',
+    '020' => 'c',
+    '040' => 'd',
+    '060' => 'b',
+    '100' => '-',
+    '120' => 'l',
+    '140' => 's'
+  }
+  print file[fil]
+end
+
+def permission_convert(pem)
+  permission = {
+    '0' => '---',
+    '1' => '--x',
+    '2' => '-w-',
+    '3' => '-wx',
+    '4' => 'r--',
+    '5' => 'r-x',
+    '6' => 'rw-',
+    '7' => 'rwx'
+  }
+  print permission[pem]
+end
+
+def l_option_output(arr)
+  print "合計#{arr.size} \n"
+  files = arr.size
+  files.times do |row|
+    fs = File.lstat(arr[row])
+    file_convert(format('%06d', fs.mode.to_s(8)).slice(0..2))
+    (3..6).each do |i|
+      permission_convert(format('%06d', fs.mode.to_s(8)).slice(i))
+    end
+    user = Etc.getpwuid(fs.uid).name
+    group = Etc.getgrgid(fs.gid).name
+    print " #{fs.nlink}".rjust(@nlinkmax)
+    print " #{user} #{group}"
+    print " #{fs.size}".rjust(@sizemax)
+    filedate = fs.atime
+    print " #{filedate.strftime('%-m月')}"
+    print " #{filedate.strftime('%d')}".rjust(2)
+    print " #{filedate.strftime('%H:%M')} #{filedate.strftime('%Y')} #{arr[row]}"
+    if fs.symlink?
+      (symlink = File.readlink(arr[row])
+       print(" -> #{symlink}"))
+    end
+    print "\n"
   end
 end
 
@@ -19,12 +83,18 @@ def output(arr)
   files.times do |row|
     col = 0
     3.times do
-      print "#{arr[row+col]} ".ljust(25)
+      print "#{arr[row + col]} ".ljust(25)
       col += files
     end
-  puts "\n"
+    puts "\n"
   end
 end
 
 arr = input(args)
-output(arr)
+count(arr)
+
+if args[:l]
+  l_option_output(arr)
+else
+  output(arr)
+end
