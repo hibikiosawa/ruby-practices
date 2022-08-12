b6a2f8ae06d13f43a05620b3bb4b67715955d134
hibikiosawa <105911942+hibikiosawa@users.noreply.github.com>
2022-08-12 11:24:48 +0900

    修正

--- 05.ls/ls2.rb
+++ 05.ls/ls2.rb
@@ -3,82 +3,75 @@
 require 'optparse'
 require 'etc'
 
-opt = OptionParser.new
-args = {}
-opt.on('-l') { |v| args[:l] = v }
-opt.parse!(ARGV)
+def main
+  opt = OptionParser.new
+  args = {}
+  opt.on('-l') { |v| args[:l] = v }
+  opt.parse!(ARGV)
+
+  arr = input(args)
+  commandline_judge(arr, args)
+end
+
+def commandline_judge(arr, args)
+  if args[:l]
+    print "合計#{arr.size} \n"
+    arr.size.times do |row|
+      l_option_output(arr, row)
+      print "\n"
+    end
+  else
+    no_option_output(arr)
+  end
+end
 
 def input(_args)
   Dir.glob('*')
 end
 
-def count(arr)
+def word_length(arr, _wordlength)
   nlinkmax = 0
   sizemax = 0
-  files = arr.size
-  files.times do |row|
+  arr.size.times do |row|
     fs = File::Stat.new(arr[row])
     nlinkmax = fs.nlink if nlinkmax < fs.nlink
     sizemax = fs.size if sizemax < fs.size
   end
-  @nlinkmax = nlinkmax.to_s.length + 1
-  @sizemax = sizemax.to_s.length + 1
+  nlinkmax = nlinkmax.to_s.length
+  sizemax = sizemax.to_s.length
+  _wordlength = nlinkmax, sizemax
 end
 
-def file_convert(fil)
+def file_convert_output(fil)
   file = {
-    '010' => 'p',
-    '020' => 'c',
-    '040' => 'd',
-    '060' => 'b',
-    '100' => '-',
-    '120' => 'l',
-    '140' => 's'
+    '010' => 'p', '020' => 'c', '040' => 'd', '060' => 'b', '100' => '-', '120' => 'l', '140' => 's'
   }
-  print file[fil]
+  print file[format('%06d', fil.to_s(8)).slice(0..2)]
 end
 
-def permission_convert(pem)
-  permission = {
-    '0' => '---',
-    '1' => '--x',
-    '2' => '-w-',
-    '3' => '-wx',
-    '4' => 'r--',
-    '5' => 'r-x',
-    '6' => 'rw-',
-    '7' => 'rwx'
-  }
-  print permission[pem]
-end
-
-def l_option_output(arr)
-  print "合計#{arr.size} \n"
-  files = arr.size
-  files.times do |row|
-    fs = File.lstat(arr[row])
-    file_convert(format('%06d', fs.mode.to_s(8)).slice(0..2))
-    (3..6).each do |i|
-      permission_convert(format('%06d', fs.mode.to_s(8)).slice(i))
-    end
-    user = Etc.getpwuid(fs.uid).name
-    group = Etc.getgrgid(fs.gid).name
-    print " #{fs.nlink}".rjust(@nlinkmax)
-    print " #{user} #{group}"
-    print " #{fs.size}".rjust(@sizemax)
-    filedate = fs.atime
-    print " #{filedate.strftime('%-m月')}"
-    print " #{filedate.strftime('%d')}".rjust(2)
-    print " #{filedate.strftime('%H:%M')} #{filedate.strftime('%Y')} #{arr[row]}"
-    if fs.symlink?
-      (symlink = File.readlink(arr[row])
-       print(" -> #{symlink}"))
-    end
-    print "\n"
+def permission_convert_output(pem)
+  permission = { '0' => '---', '1' => '--x', '2' => '-w-', '3' => '-wx', '4' => 'r--', '5' => 'r-x', '6' => 'rw-',
+                 '7' => 'rwx' }
+  (3..6).each do |i|
+    print permission[(format('%06d', pem.to_s(8)).slice(i))]
   end
 end
 
-def output(arr)
+def l_option_output(arr, row)
+  wordlength = word_length(arr, wordlength)
+  fs = File.lstat(arr[row])
+  file_convert_output(fs.mode)
+  permission_convert_output(fs.mode)
+  user = Etc.getpwuid(fs.uid).name
+  group = Etc.getgrgid(fs.gid).name
+  nlink = fs.nlink.to_s.rjust(wordlength[2].to_i)
+  filesize = fs.size.to_s.rjust(wordlength[1])
+  file = fs.atime.strftime('%-m月 %d %H:%M %Y')
+  print " #{nlink} #{user} #{group} #{filesize} #{file} #{arr[row]}"
+  print(" -> #{File.readlink(arr[row])}") if fs.symlink?
+end
+
+def no_option_output(arr)
   files = ((arr.size + 1).to_f / 3).ceil
   files.times do |row|
     col = 0
@@ -90,11 +83,5 @@ def output(arr)
   end
 end
 
-arr = input(args)
-count(arr)
+main
 
-if args[:l]
-  l_option_output(arr)
-else
-  output(arr)
-end
