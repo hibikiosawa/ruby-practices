a09a20b98b83b568137fcbfc3b80a0070a323e8c
osawa <hibiki2_627@yahoo.co.jp>
2022-08-30 18:26:21 +0900

    rubocop対応のためメソッド追加

--- 08.ls_object/ls_wc.rb
+++ 08.ls_object/ls_wc.rb
@@ -47,7 +47,7 @@ def files_line_word_total_count(files)
   [lines_size, words_size, files_size]
 end
 
-def total_output(files, option)
+def output_total(files, option)
   lines_size, words_size, files_size = files_line_word_total_count(files)
   print lines_size.to_s.rjust(8) if option['l']
   print words_size.to_s.rjust(8) if option['w']
@@ -60,20 +60,24 @@ def total_output(files, option)
   print " total \n" if files.size > 1
 end
 
+def output_file_info(file_readed, file, option)
+  print count_lines(file_readed).to_s.rjust(8) if option['l']
+  print count_words(file_readed).to_s.rjust(8) if option['w']
+  print file_readed.size.to_s.rjust(8) if option['c']
+  if option['l'] == false && option['w'] == false && option['c'] == false
+    print count_lines(file_readed).to_s.rjust(8)
+    print count_words(file_readed).to_s.rjust(8)
+    print file_readed.size.to_s.rjust(8)
+  end
+  print " #{file} \n"
+end
+
 def output_standard(files, option)
   files.each do |file|
     file_readed = File.read(file)
-    print count_lines(file_readed).to_s.rjust(8) if option['l']
-    print count_words(file_readed).to_s.rjust(8) if option['w']
-    print file_readed.size.to_s.rjust(8) if option['c']
-    if option['l'] == false && option['w'] == false && option['c'] == false
-      print count_lines(file_readed).to_s.rjust(8)
-      print count_words(file_readed).to_s.rjust(8)
-      print file_readed.size.to_s.rjust(8)
-    end
-    print " #{file} \n"
+    output_file_info(file_readed, file, option)
   end
-  total_output(files, option) if files.size > 1
+  output_total(files, option) if files.size > 1
 end
 
 def output_stdin(stdin, option)
