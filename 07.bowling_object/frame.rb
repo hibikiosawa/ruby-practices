8f6f788fcf38cc04f2d232fd4ba68b9dc742ddbb
osawa <hibiki2_627@yahoo.co.jp>
2022-09-14 16:16:31 +0900

    最大スコアの定数を作成

--- 07.bowling_object/frame.rb
+++ 07.bowling_object/frame.rb
@@ -5,6 +5,8 @@ require_relative 'shot'
 class Frame
   attr_reader :first_shot, :second_shot
 
+  MAX_SCORE = 10
+
   def initialize(first_mark, second_mark)
     @first_shot = Shot.new(first_mark)
     @second_shot = Shot.new(second_mark)
@@ -15,10 +17,10 @@ class Frame
   end
 
   def strike?
-    @first_shot.score == 10
+    @first_shot.score == MAX_SCORE
   end
 
   def spare?
-    [@first_shot.score, @second_shot.score].sum == 10
+    [@first_shot.score, @second_shot.score].sum == MAX_SCORE
   end
 end
