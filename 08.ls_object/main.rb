e1349d55f9bd9d4dad4aaf05b3d74a84b88ccff8
osawa <hibiki2_627@yahoo.co.jp>
2022-09-20 11:54:11 +0900

    オプションの判定を追加

--- 08.ls_object/main.rb
+++ 08.ls_object/main.rb
@@ -1,5 +1,18 @@
-class main
+require 'optparse'
+
+class Main
+
   def initialize
-    
+    option = ARGV.getopts('arl')
+    option_judge(option)
   end
+
+  def option_judge(option)
+    all = true if option['a'] 
+    reverse = true if option['r']
+    long_format = true if option['l']
+  end
+
 end
+
+Main.new
